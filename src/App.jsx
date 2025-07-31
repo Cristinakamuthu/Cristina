923b9db566902ec0de781bf4d7131d614f94f9ca
Cristina <kamuthucristina@gmail.com>
2025-07-31 21:21:07 +0300

    completed the home page

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,12 @@ import Home from './components/Home'
 import { Outlet } from 'react-router-dom'
 
 function App() {
- 
-
   return (
-    <>
-  
-  <Outlet />
-    </>
-  )
+    <div>
+      <Outlet />
+    </div>
+  );
 }
+
 
 export default App
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,20 @@
-import React from 'react' 
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
+import backgroundImg from '../assets/cristina.jpeg'; // adjust path if needed
 
 const Home = () => {
   return (
-    <div>
-    <header>
-      <Navbar/>
-    </header>
-    </div>
-  )
-}
+    <div className="home-page">
+      <Navbar />
+      <div className="hero-section">
+        <h1 className="cristina">CRISTINA</h1>
 
-export default Home
+        <img src={backgroundImg} alt="Cristina" className="center-image" />
+
+        <h1 className="kamuthu">KAMUTHU</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
